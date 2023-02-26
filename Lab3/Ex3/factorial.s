a7bc19ba18a26da8e722ed7100da9a9bda9fc026
zsc2003 <zhoushch@shanghaitech.edu.cn>
2023-02-26 16:52:21 +0800

    exploring

--- Lab3/Ex3/factorial.s
+++ Lab3/Ex3/factorial.s
@@ -10,7 +10,7 @@ main_loop:
 	beq t0, t1, main_exit
 	slli t3, t0, 2
 	add t4, t2, t3
-	lw a0, 0(t4)
+	lw a0, 0(t4) # input data
 
 	addi sp, sp, -20
 	sw t0, 0(sp)
@@ -28,12 +28,12 @@ main_loop:
 	lw t4, 16(sp)
 	addi sp, sp, 20
 
-	addi a1, a0, 0
+	addi a1, a0, 0 # use a0 to store answer
 	addi a0, x0, 1
-	ecall # Print Result
+	ecall # Print Result using a1
 	addi a1, x0, ' '
 	addi a0, x0, 11
-	ecall
+	ecall # print ' ' using a1
 	
 	addi t0, t0, 1
 	jal x0, main_loop
@@ -43,9 +43,8 @@ main_exit:
 
 # calculate factorial
 factorial:
-	addi t0, x0, 1
-	addi t1, x0, 1
-	beq t0, a0, main_loop
-	mul t1, t1, t0
-	addi t0, t0, 1
-	jal x0, factorial
+	addi a0, a0, -1
+	beq a0, x0, L1
+
+L1:
+	ret

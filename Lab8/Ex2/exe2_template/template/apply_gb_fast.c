057fa7c1e08ad8e6ebeb111eaec22fc7202aa37d
wangph1 <wangph1@shanghaitech.edu.cn>
2023-04-08 16:55:04 +0800

    exe2 finish

--- Lab8/Ex2/exe2_template/template/apply_gb_fast.c
+++ Lab8/Ex2/exe2_template/template/apply_gb_fast.c
@@ -18,10 +18,39 @@ Image transpose(Image a){
 // use the function transpose() to implement the vertical gaussian blur in a fast way
 Image apply_gb(Image a, FVec gv)
 {
+    struct timeval start_time, stop_time, elapsed_time; 
+    gettimeofday(&start_time,NULL);
+
     Image b = gb_h(a, gv);
+
+    gettimeofday(&stop_time,NULL);
+    timersub(&stop_time, &start_time, &elapsed_time); 
+    printf("horizontal gaussian blur time: %f \n", elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0);
+
+    gettimeofday(&start_time,NULL);
+
     Image c = transpose(b);
+
+    gettimeofday(&stop_time,NULL);
+    timersub(&stop_time, &start_time, &elapsed_time); 
+    printf("First transpose time: %f \n", elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0);
+    
+    gettimeofday(&start_time,NULL);
+
     c = gb_h(c, gv);
+
+    gettimeofday(&stop_time,NULL);
+    timersub(&stop_time, &start_time, &elapsed_time); 
+    printf("vertical gaussian blur time: %f \n", elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0);
+    
+    gettimeofday(&start_time,NULL);
+
     c = transpose(c);
+
+    gettimeofday(&stop_time,NULL);
+    timersub(&stop_time, &start_time, &elapsed_time); 
+    printf("Second transpose time: %f \n", elapsed_time.tv_sec+elapsed_time.tv_usec/1000000.0);
+
     free(b.data);
     return c;
 }

86c30ac5ef9821a73e7284e984fbcde4570bf9b2
zhoushch <zhoushch@shanghaitech.edu.cn>
2023-04-22 00:16:50 +0800

    init

---
+++ Lab9/part1.c
@@ -0,0 +1,45 @@
+#include <emmintrin.h> /* header file for the SSE intrinsics */
+#include <assert.h>
+
+int **a;
+int **b;
+int **c;
+
+void naive_add(void) {
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 40; ++j) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+void simd_add(void) {
+    // TODO: implement me!
+}
+
+int main(void) {
+    a = malloc(1000 * sizeof(int *));
+    b = malloc(1000 * sizeof(int *));
+    c = malloc(1000 * sizeof(int *));
+    for (int i = 0; i < 1000; ++i) {
+        a[i] = malloc(40 * sizeof(int));
+        b[i] = malloc(40 * sizeof(int));
+        c[i] = malloc(40 * sizeof(int));
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 40; ++j) {
+            a[i][j] = i * 40 + j;
+            b[i][j] = 2 * (i * 40 - j);
+        }
+    }
+
+    simd_add();
+
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 40; ++j) {
+            assert(c[i][j] == (i * 40 + j + 2 * (i * 40 - j)));
+        }
+    }
+    return 0;
+}
